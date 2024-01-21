c6d9d55df438a0b1cdb8355999a95a546ff3d394
SirMunyex <148867612+SirMunyex@users.noreply.github.com>
2024-01-21 15:58:01 +1100

    Recent commit

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ $(function () {
         $('#navbar-close').toggleClass('hidden');
     });
 });
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
