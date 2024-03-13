5dc96bcefd7306a02f2e9e7f6377965f84414756
SirMunyex <148867612+SirMunyex@users.noreply.github.com>
2024-03-13 16:43:19 +1100

    Added your product items onto the product catalogue

--- catalogData.js
+++ catalogData.js
@@ -4,65 +4,40 @@ const catalogSections = [
     {
         name: 'T-Shirt Printing',
         items: [
-            { name: 'Heat Transfer Vinyl', description: 'Description for Item 1', price: '$10.00', image: 'Description for Item A', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Direct to Film', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Full Sublimation', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Screen Printing', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Embroidery', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
+            { name: 'Branded Stationery', description: 'Branded Stationery', price: 'from $10.00', image: './resources/product-images/branded-stationery.jpg' },
+            { name: 'Corporate Gifts', description: 'Corporate Gifts', price: 'from $10.00', image: './resources/product-images/corporate-gifts.jpg' },
+            { name: 'Corporate Wear', description: 'Corporate Wear', price: 'from $10.00', image: './resources/product-images/corporate-wear.jpg' },
+            { name: 'Backdrop Banner', description: 'Backdrop Banner', price: '$320.00', image: './resources/product-images/backdrop-banner.jpg' },
+            { name: 'Backdrop Banner', description: 'Economy', price: '$250.00', image: './resources/product-images/economy-backdrop-banner.jpg' },
+            { name: 'Folding Banner', description: 'Folding Banner', price: '$170.00', image: './resources/product-images/display-table.jpg' },
+            { name: 'Fountain Banner', description: 'Fountain Banner', price: '$500.00', image: './resources/product-images/fountain-banner.jpg' },
+            { name: 'Parasol Banner', description: 'Parasol Banner', price: '$250.00', image: './resources/product-images/parasol-banner.jpg' },
+            { name: 'Socks Banner', description: 'Socks Banner', price: '$250.00', image: './resources/product-images/socks-banner.jpg' },
+            { name: 'Lantern Banner', description: 'Lantern Banner', price: '$220.00', image: './resources/product-images/lantern-banner.jpg' },
+            { name: 'Pop-up Banner', description: 'Pop-up Banner', price: '$100.00', image: './resources/product-images/pop-up-banner.jpg' },
+            { name: 'Snake Banner', description: 'Snake Banner', price: '$110.00', image: './resources/product-images/snake-banner.jpg' },
+            { name: 'Telescopic Banner', description: 'Telescopic Banner', price: '$65.00', image: './resources/product-images/telescopic-banner.jpg' },
+            { name: 'Teardrop Banner', description: 'Teardrop Banner', price: '$65.00', image: './resources/product-images/teardrop-banner.jpg' },
+            { name: 'Pull-up Banner', description: 'Pull-up Banner', price: '$85.00', image: './resources/product-images/pull-up-banner.jpg' },
+            { name: 'Promo Counter', description: 'Promo Counter', price: '$80.00', image: './resources/product-images/promo-counter.jpg' },
+            { name: 'X-Frame Banner', description: 'X-Frame Banner', price: '$30.00', image: './resources/product-images/x-frame-banner.jpg' },
+            { name: 'Pop-Up Banner', description: 'Pop-Up Banner', price: '$100.00', image: './resources/product-images/pop-up-banner-ii.jpg' },
             // Add more items for Catalog Section 1
         ]
     },
-    {
-        name: 'Signage',
-        items: [
-            { name: 'Window Graphics', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/window-signage.jpg' },
-            { name: 'Street & Directional Signs', description: 'Description for Item B', price: '$25.00', image: './resources/product-images/wall-signage.jpg' },
-            { name: 'Office Signage', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Premise Branding', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/window-signage.jpg' },
-            { name: 'Wall Branding', description: 'Description for Item B', price: '$25.00', image: './resources/product-images/wall-signage.jpg' },
-            { name: '3d Signs', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Aclyaric Signs', description: 'Description for Item A', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            // Add more items for Catalog Section 2
-        ]
-    },
+
+
+
     // Add more catalog sections if needed
-    {
-        name: 'Car Branding',
-        items: [
-            { name: 'Vinyl Lettering', description: 'Vehicle Lettering', price: '$20.00', image: '../resources/product-images/vehicle-stickers.jpg' },
-            { name: 'Partial Wrap', description: 'Vehicle Wrap', price: '$25.00', image: './resources/product-images/vehicle-wrapping.jpg' },
-            { name: 'Full Wrap', description: 'One Way Vision', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Magnetic Stickers', description: 'Description for Item B', price: '$25.00', image: './resources/product-images/placeholder-image.png' },
-            // Add more items for Catalog Section 2
-        ]
-    },
-    // Add more catalog sections if needed
-    {
-        name: 'Exhibition Banners',
-        items: [
-            { name: 'Teardrop/Telescopic Banners', description: 'Vehicle Lettering', price: '$20.00', image: '../resources/product-images/vehicle-stickers.jpg' },
-            { name: 'Pop-up Banners', description: 'Vehicle Wrap', price: '$25.00', image: './resources/product-images/vehicle-wrapping.jpg' },
-            { name: 'Gazebo', description: 'One Way Vision', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'S Banners', description: 'Description for Item B', price: '$25.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Backdrop Banners', description: 'Vehicle Lettering', price: '$20.00', image: '../resources/product-images/vehicle-stickers.jpg' },
-            { name: 'Pull-up Banners', description: 'Vehicle Wrap', price: '$25.00', image: './resources/product-images/vehicle-wrapping.jpg' },
-            { name: 'X-Frame Banners', description: 'One Way Vision', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Table Cloths', description: 'Description for Item B', price: '$25.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Lantern Banners', description: 'Vehicle Lettering', price: '$20.00', image: '../resources/product-images/vehicle-stickers.jpg' },
-            { name: 'Exhibition Counters', description: 'Vehicle Wrap', price: '$25.00', image: './resources/product-images/vehicle-wrapping.jpg' },
-            // Add more items for Catalog Section 2
-        ]
-    },
-    // Add more catalog sections if needed
-    {
-        name: 'Clothing',
-        items: [
-            { name: 'Safety Wear', description: 'Vehicle Lettering', price: '$20.00', image: '../resources/product-images/vehicle-stickers.jpg' },
-            { name: 'Corporate Wear', description: 'Vehicle Wrap', price: '$25.00', image: './resources/product-images/vehicle-wrapping.jpg' },
-            { name: 'Sports Wear', description: 'One Way Vision', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
-            { name: 'Promotional Wear', description: 'Description for Item B', price: '$25.00', image: './resources/product-images/placeholder-image.png' },
-            // Add more items for Catalog Section 2
-        ]
-    },
+    // {
+    // name: 'Clothing',
+    // items: [
+    //     { name: 'Safety Wear', description: 'Vehicle Lettering', price: '$20.00', image: '../resources/product-images/vehicle-stickers.jpg' },
+    //     { name: 'Corporate Wear', description: 'Vehicle Wrap', price: '$25.00', image: './resources/product-images/vehicle-wrapping.jpg' },
+    //     { name: 'Sports Wear', description: 'One Way Vision', price: '$20.00', image: './resources/product-images/placeholder-image.png' },
+    //     { name: 'Promotional Wear', description: 'Description for Item B', price: '$25.00', image: './resources/product-images/placeholder-image.png' },
+    // Add more items for Catalog Section 2
+    //     ]
+    // },
     // Add more catalog sections if needed
 ];
